b33851a3f11042b75ac2aa69d6d21c276d8bea80
SIAOYUCHEN <Strawberry@bloxmith.com>
2023-05-03 13:04:26 +0800

    [feat][Add ProType And UnitTest In Project]
    1. Add ReadMe

---
+++ Protype/Protype/ProTypeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Protype
+{
+    [TestFixture]
+    public class ProTypeTest
+    {
+        [Test]
+        public void Test1()
+        {
+            ConcretePrototype1 p1 = new ConcretePrototype1("I");
+            var c1 = (ConcretePrototype1) p1.Clone();
+
+            var actual = c1.Id;
+
+            Assert.AreEqual("I", actual);
+            
+            ConcretePrototype2 p2 = new ConcretePrototype2("II");
+            ConcretePrototype2 c2 = (ConcretePrototype2) p2.Clone();
+            
+            actual = c2.Id;
+            Assert.AreEqual("II", actual);
+        }
+    }
+}
---
+++ Protype/Protype/Prototype.cs
@@ -0,0 +1,19 @@
+﻿namespace Protype
+{
+    public abstract class Prototype
+    {
+        private readonly string _id;
+
+        protected Prototype(string id)
+        {
+            _id = id;
+        }
+
+        public string Id
+        {
+            get { return _id; }
+        }
+
+        public abstract Prototype Clone();
+    }
+}
