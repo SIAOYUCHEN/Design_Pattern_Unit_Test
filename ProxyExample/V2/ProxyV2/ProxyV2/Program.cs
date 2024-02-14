a36f0b9bd52ef7da5e39410ef4982652a1d94004
SIAOYUCHEN <Strawberry@bloxmith.com>
2024-02-14 11:55:09 +0800

    [feat][Add Proxy Design Pattern V2 Version]

---
+++ ProxyExample/V2/ProxyV2/ProxyV2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using ProxyV1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExpenseTrackingCli cli = new ExpenseTrackingCli(new VirtualSuperExpenseTrackingSystemProxy());
+        cli.Start();
+    }
+}
---
+++ ProxyExample/V2/ProxyV2/ProxyV2/VirtualSuperExpenseTrackingSystemProxy.cs
@@ -0,0 +1,38 @@
+﻿namespace ProxyV1;
+
+public class VirtualSuperExpenseTrackingSystemProxy : IExpenseTrackingSystem
+{
+    private SuperExpenseTrackingSystem _superSystem;
+
+    public List<Expense> GetExpenses()
+    {
+        LazyInitialization();
+        return _superSystem.GetExpenses();
+    }
+
+    public void AddExpense(Expense expense)
+    {
+        LazyInitialization();
+        _superSystem.AddExpense(expense);
+    }
+
+    public void EditExpense(Expense expense)
+    {
+        LazyInitialization();
+        _superSystem.EditExpense(expense);
+    }
+
+    public void ExportCSV(string filename)
+    {
+        LazyInitialization();
+        _superSystem.ExportCSV(filename);
+    }
+
+    private void LazyInitialization()
+    {
+        if (_superSystem == null)
+        {
+            _superSystem = new SuperExpenseTrackingSystem();
+        }
+    }
+}
